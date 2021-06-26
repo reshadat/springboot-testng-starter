728c7f9e8bfb95483d9edd878c91cb9142651151
Reshadat.Ali <Reshadat.Ali@sophos.com>
2021-06-26 21:31:56 +0530

    Removed post construct and pre destroy as deprecated in Java 9

--- src/main/java/com/reshadat/springbootTestNGStarter/testNG/service/HelloWorldService.java
+++ src/main/java/com/reshadat/springbootTestNGStarter/testNG/service/HelloWorldService.java
@@ -1,27 +1,26 @@
 package com.reshadat.springbootTestNGStarter.testNG.service;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 @Service
-public class HelloWorldService {
+public class HelloWorldService implements InitializingBean, DisposableBean {
 
     public int getValueOfB() {
         return 10;
     }
 
-
-    @PostConstruct
-    void init() {
-        String result = "Anything you do in PostConstruct will run only once, just after the initialization of bean properties";
+    @Override
+    public void destroy() {
+        String result = "Anything you do in destroy will run only once, just before Spring removes our bean from the application context";
         System.out.println(result);
     }
 
-    @PreDestroy
-    void cleanup() {
-        String result = "Anything you do in PreDestroy will run only once, just before Spring removes our bean from the application context";
+    @Override
+    public void afterPropertiesSet(){
+        String result = "Anything you do in afterPropertiesSet will run only once, just after the initialization of bean properties";
         System.out.println(result);
+
     }
 }
