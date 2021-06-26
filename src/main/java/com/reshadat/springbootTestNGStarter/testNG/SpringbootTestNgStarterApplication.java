70c43c0bda31fd6218378a906b15fbfe568dfe10
Reshadat.Ali <Reshadat.Ali@sophos.com>
2021-06-26 21:13:38 +0530

    adding post construct and pre destroy

--- src/main/java/com/reshadat/springbootTestNGStarter/testNG/SpringbootTestNgStarterApplication.java
+++ src/main/java/com/reshadat/springbootTestNGStarter/testNG/SpringbootTestNgStarterApplication.java
@@ -3,6 +3,8 @@ package com.reshadat.springbootTestNGStarter.testNG;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringbootTestNgStarterApplication {
 
--- src/main/java/com/reshadat/springbootTestNGStarter/testNG/service/HelloWorldService.java
+++ src/main/java/com/reshadat/springbootTestNGStarter/testNG/service/HelloWorldService.java
@@ -2,11 +2,26 @@ package com.reshadat.springbootTestNGStarter.testNG.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service
 public class HelloWorldService {
 
-   public int getValueOfB(){
+    public int getValueOfB() {
         return 10;
     }
 
+
+    @PostConstruct
+    void init() {
+        String result = "Anything you do in PostConstruct will run only once, just after the initialization of bean properties";
+        System.out.println(result);
+    }
+
+    @PreDestroy
+    void cleanup() {
+        String result = "Anything you do in PreDestroy will run only once, just before Spring removes our bean from the application context";
+        System.out.println(result);
+    }
 }
